6ae2cb5bda41bbecd4a5fbb30df2d180647c6d39
Guilherme Coutinho <guilhermecoutinho579@gmail.com>
2023-05-30 20:40:42 -0300

    isola o estado

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { store } from "./store.js";
+import {adicionar, itens } from "./store.js";
 
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
@@ -11,7 +11,7 @@ function envia(evento) {
     evento.preventDefault();
     console.log('Formulário Enviado!');
     const n = form.valor.value;
-    store.estado.push(n);
+    adicionar(n);
     form.valor.value = "";
     form.valor.focus();
     atualiza();
@@ -20,9 +20,9 @@ function envia(evento) {
 function atualiza() {
     const ol = document.querySelector('ol');
     ol.innerHTML = "";
-    for(let i = 0; i < store.estado.length; i++) {
+    for(let i = 0; i < itens().length; i++) {
         const li = document.createElement('li');
-        li.textContent = store.estado[i];
+        li.textContent = itens()[i];
         ol.appendChild(li);
     }
 }
